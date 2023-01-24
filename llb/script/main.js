704f33c1908a88202b7aa1d044a832995ac98bfe
Simidaniels <simidaniels0506@gmail.com>
2023-01-24 05:47:42 +0100

    finalised

--- llb/script/main.js
+++ llb/script/main.js
@@ -1,1 +1,9 @@
 alert('Welcome to designBrooks!');
+
+var docTitle = document.title;
+window.addEventListener ('blur',()=>{
+    document.title = 'Design Brooks'
+})
+window.addEventListener('focus',()=>{
+    document.title = docTitle
+})
